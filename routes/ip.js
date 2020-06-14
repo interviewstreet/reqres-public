c556425c0c20473bfa0a642814172f74386a6714
Myroslava Stavnycha <stavnycha.m@gmail.com>
2020-06-14 07:40:51 +0300

    formatting

--- routes/ip.js
+++ routes/ip.js
@@ -4,14 +4,16 @@ var data = require("./../data.json");
 
 module.exports = {
     get: function(req, res, next) {
-    	var ip = _.find(data.ip, function(ip_info) {
-    		return ip_info.ip == req.params.ip
-    	});
+        var ip = _.find(data.ip, function(ip_info) {
+            return ip_info.ip == req.params.ip
+        });
 
-    	if (ip) {
-			return res.status(200).send({data: ip});
-    	} else {
-    		return res.status(404).send({});
-    	}
+        if (ip) {
+            return res.status(200).send({
+                data: ip
+            });
+        } else {
+            return res.status(404).send({});
+        }
     }
 };
